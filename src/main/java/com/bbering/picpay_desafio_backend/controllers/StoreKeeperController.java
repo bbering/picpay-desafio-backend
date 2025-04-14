0f610ba5b91ea9d979bfd5db96d5e164d9d879e8
bbering <bbering88@gmail.com>
2025-04-14 00:04:00 -0300

    :sparkles: feat: criada StoreKeeperController REST com todos os metodos CRUD

--- src/main/java/com/bbering/picpay_desafio_backend/controllers/StoreKeeperController.java
+++ src/main/java/com/bbering/picpay_desafio_backend/controllers/StoreKeeperController.java
@@ -3,9 +3,11 @@ package com.bbering.picpay_desafio_backend.controllers;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -29,9 +31,22 @@ public class StoreKeeperController {
         return new ResponseEntity<>(skResponse, HttpStatus.OK);
     }
 
-    @PostMapping("/sk/save")
+    @PostMapping("/sk")
     public ResponseEntity<?> createNewStoreKeeper(@Valid @RequestBody StoreKeeperRequestDTO skDTO) {
         StoreKeeperResponseDTO skResponse = skService.createStoreKeeper(skDTO);
+        return new ResponseEntity<>(skResponse, HttpStatus.CREATED);
+    }
+
+    @DeleteMapping("/sk/{id}")
+    public ResponseEntity<?> deleteStoreKeeper(@PathVariable Long id) {
+        skService.deleteStoreKeeper(id);
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+
+    @PutMapping("/sk/{id}")
+    public ResponseEntity<?> updateStoreKeeperData(@Valid @RequestBody StoreKeeperRequestDTO skDTO,
+            @PathVariable Long id) {
+        StoreKeeperResponseDTO skResponse = skService.updateStoreKeeper(skDTO, id);
         return new ResponseEntity<>(skResponse, HttpStatus.OK);
     }
 }
