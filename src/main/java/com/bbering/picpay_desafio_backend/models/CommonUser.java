4920ea917bff8a50a2dfaa631a62b8e7c145381c
bbering <bbering88@gmail.com>
2025-04-21 21:43:50 -0300

    feat(CommonUser): adicionado atributo balance para model

--- src/main/java/com/bbering/picpay_desafio_backend/models/CommonUser.java
+++ src/main/java/com/bbering/picpay_desafio_backend/models/CommonUser.java
@@ -1,5 +1,7 @@
 package com.bbering.picpay_desafio_backend.models;
 
+import java.math.BigDecimal;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
@@ -44,4 +46,6 @@ public class CommonUser {
     @Column(name = "password", nullable = false)
     private String password;
 
+    @Column(name = "balance")
+    private BigDecimal balance = BigDecimal.ZERO;
 }
