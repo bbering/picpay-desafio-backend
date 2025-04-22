33d29025cd0c46ca44c9c61bbaa0d5828b2300b5
bbering <bbering88@gmail.com>
2025-04-21 22:39:49 -0300

    refactor(TransactionRequestDTO): ajuste na DTO de requests para Transactions, removidas as notações JPA que foram adicionadas a classe incorretamente

---
+++ src/main/java/com/bbering/picpay_desafio_backend/services/TransactionsService.java
@@ -0,0 +1,43 @@
+package com.bbering.picpay_desafio_backend.services;
+
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.bbering.picpay_desafio_backend.dtos.TransactionRequestDTO;
+import com.bbering.picpay_desafio_backend.models.CommonUser;
+import com.bbering.picpay_desafio_backend.models.StoreKeeper;
+import com.bbering.picpay_desafio_backend.models.Transactions;
+import com.bbering.picpay_desafio_backend.repositories.CommonUserRepository;
+import com.bbering.picpay_desafio_backend.repositories.StoreKeeperRepository;
+import com.bbering.picpay_desafio_backend.repositories.TransactionsRepository;
+
+@Service
+public class TransactionsService {
+
+    @Autowired
+    private TransactionsRepository transactionsRepository;
+
+    @Autowired
+    private CommonUserRepository commonUserRepository;
+
+    @Autowired
+    private StoreKeeperRepository storeKeeperRepository;
+
+    public Transactions toEntity(TransactionRequestDTO trnRequestDTO) {
+        Transactions transaction = new Transactions();
+        BeanUtils.copyProperties(trnRequestDTO, transaction);
+
+        CommonUser sender = commonUserRepository.findById(trnRequestDTO.getSenderId())
+                .orElseThrow(() -> new RuntimeException("Nenhum usuário encontrado com o id"));
+
+        StoreKeeper receiver = storeKeeperRepository.findById(trnRequestDTO.getReceiverId())
+                .orElseThrow(() -> new RuntimeException("Nenhum lojista encontrado com o id"));
+
+        transaction.setSender(sender);
+        transaction.setReceiver(receiver);
+
+        return transaction;
+    }
+
+}
