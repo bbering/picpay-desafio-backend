79c8887663b6314a0b25f7b92141c5b138f2199c
bbering <bbering88@gmail.com>
2025-04-21 22:18:53 -0300

    :sparkles: feat: adicionada nova model Transactions para rastrear transações

---
+++ src/main/java/com/bbering/picpay_desafio_backend/models/Transactions.java
@@ -0,0 +1,47 @@
+package com.bbering.picpay_desafio_backend.models;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "transactions")
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@EqualsAndHashCode
+public class Transactions {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "transaction_amount")
+    private BigDecimal amount;
+
+    @Column(name = "timestamp")
+    private LocalDateTime timestamp;
+
+    @ManyToOne
+    @JoinColumn(name = "sender_id")
+    private CommonUser sender;
+
+    @ManyToOne
+    @JoinColumn(name = "receiver_id")
+    private StoreKeeper receiver;
+
+}
