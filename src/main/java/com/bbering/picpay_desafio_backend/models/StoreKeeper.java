02697d3450f7b55a3abfc9a6549e6613bd726675
bbering <bbering88@gmail.com>
2025-04-23 22:42:58 -0300

    :recycle: refactor(StoreKeeper): removidos codigos repetitivos de StoreKeeper, agora presentes na super classe User

--- src/main/java/com/bbering/picpay_desafio_backend/models/StoreKeeper.java
+++ src/main/java/com/bbering/picpay_desafio_backend/models/StoreKeeper.java
@@ -20,32 +20,13 @@ import lombok.Setter;
 @Table(name = "storeKeepers")
 @AllArgsConstructor
 @NoArgsConstructor
-@EqualsAndHashCode
+@EqualsAndHashCode(callSuper = true)
 @Getter
 @Setter
-public class StoreKeeper {
-
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "id")
-    private Long id;
-
-    @Column(name = "name", nullable = false)
-    @Size(min = 3, max = 100, message = "O nome deve possuir no mínimo 3 e no máximo 100 caracteres")
-    private String fullName;
+public class StoreKeeper extends User {
 
     @Column(name = "CNPJ", nullable = false, unique = true)
     @Size(min = 14, max = 14, message = "O CNPJ deve ter obrigatoriamente 14 dígitos")
     @Pattern(regexp = "^[0-9]{14}$", message = "O CNPJ deve conter exatamente 14 dígitos numéricos.")
     private String cnpj;
-
-    @Column(name = "email", nullable = false, unique = true)
-    @Size(max = 256, message = "O email deve possuir, no máximo, 256 caracteres")
-    private String email;
-
-    @Column(name = "password", nullable = false)
-    private String password;
-
-    @Column(name = "balance")
-    private BigDecimal balance = BigDecimal.ZERO;
 }
