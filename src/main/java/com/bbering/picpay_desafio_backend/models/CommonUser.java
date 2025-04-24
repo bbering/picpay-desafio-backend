cd1c35e0f77efd5558d9c8df14577e6ddce48210
bbering <bbering88@gmail.com>
2025-04-23 22:44:12 -0300

    :recycle: refactor(CommonUser): removidos atributos repetitivos em CommonUser que estão agora na classe mãe User

--- src/main/java/com/bbering/picpay_desafio_backend/models/CommonUser.java
+++ src/main/java/com/bbering/picpay_desafio_backend/models/CommonUser.java
@@ -1,12 +1,7 @@
 package com.bbering.picpay_desafio_backend.models;
 
-import java.math.BigDecimal;
-
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
@@ -20,32 +15,14 @@ import lombok.Setter;
 @Table(name = "commonUsers")
 @AllArgsConstructor
 @NoArgsConstructor
-@EqualsAndHashCode
+@EqualsAndHashCode(callSuper = true)
 @Getter
 @Setter
-public class CommonUser {
-
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "id")
-    private Long id;
-
-    @Column(name = "name", nullable = false)
-    @Size(min = 3, max = 100, message = "O nome deve possuir no mínimo 3 e no máximo 100 caracteres")
-    private String fullName;
+public class CommonUser extends User {
 
     @Column(name = "CPF", nullable = false, unique = true)
     @Size(min = 11, max = 11, message = "O CPF deve ter obrigatóriamente 11 dígitos")
     @Pattern(regexp = "^[0-9]{11}$", message = "O CPF deve conter exatamente 11 dígitos numéricos.")
     private String CPF;
 
-    @Column(name = "email", nullable = false, unique = true)
-    @Size(max = 256, message = "O email deve possuir, no máximo, 256 caracteres")
-    private String email;
-
-    @Column(name = "password", nullable = false)
-    private String password;
-
-    @Column(name = "balance")
-    private BigDecimal balance = BigDecimal.ZERO;
 }
--- src/main/java/com/bbering/picpay_desafio_backend/models/StoreKeeper.java
+++ src/main/java/com/bbering/picpay_desafio_backend/models/StoreKeeper.java
@@ -1,12 +1,7 @@
 package com.bbering.picpay_desafio_backend.models;
 
-import java.math.BigDecimal;
-
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
