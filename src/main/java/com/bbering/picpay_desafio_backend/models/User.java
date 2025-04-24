66a010b799e8b16256cc15e4321c20ff33995556
bbering <bbering88@gmail.com>
2025-04-23 22:40:53 -0300

    :sparkles: feat: criação de superclasse User para reduzir a quantidade de código repetitivo nas entidades de usuario comum e lojista

---
+++ src/main/java/com/bbering/picpay_desafio_backend/models/User.java
@@ -0,0 +1,44 @@
+package com.bbering.picpay_desafio_backend.models;
+
+import java.math.BigDecimal;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.MappedSuperclass;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@MappedSuperclass
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@EqualsAndHashCode
+public abstract class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    protected Long id;
+
+    @Column(name = "name", nullable = false)
+    @Size(min = 3, max = 100, message = "O nome deve possuir no mínimo 3 e no máximo 100 caracteres")
+    protected String fullName;
+
+    @Column(name = "email", nullable = false, unique = true)
+    @Size(max = 256, message = "O email deve possuir, no máximo, 256 caracteres")
+    protected String email;
+
+    @Column(name = "password", nullable = false)
+    protected String password;
+
+    @Column(name = "balance")
+    protected BigDecimal balance = BigDecimal.ZERO;
+
+}
