bf6b22fb05d680686cbf15af2ffb54a6aa5279ce
bbering <bbering88@gmail.com>
2025-04-24 23:41:13 -0300

    feat(TransactionsService): adicionado metodo auxiliar para simular uma validação de transação com serviço externo

--- src/main/java/com/bbering/picpay_desafio_backend/services/TransactionsService.java
+++ src/main/java/com/bbering/picpay_desafio_backend/services/TransactionsService.java
@@ -2,8 +2,11 @@ package com.bbering.picpay_desafio_backend.services;
 
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 
+import com.bbering.picpay_desafio_backend.dtos.AuthorizationResponseDTO;
 import com.bbering.picpay_desafio_backend.dtos.TransactionRequestDTO;
 import com.bbering.picpay_desafio_backend.dtos.TransactionResponseDTO;
 import com.bbering.picpay_desafio_backend.models.CommonUser;
@@ -25,6 +28,9 @@ public class TransactionsService {
 
     @Autowired
     private StoreKeeperRepository storeKeeperRepository;
+
+    @Autowired
+    private RestTemplate restTemplate;
 
     public Transactions toEntity(TransactionRequestDTO trnRequestDTO) {
         Transactions transaction = new Transactions();
@@ -65,4 +71,22 @@ public class TransactionsService {
         };
     }
 
+    public Boolean validateTransaction() {
+        String url = "https://util.devi.tools/api/v2/authorize";
+
+        try {
+            ResponseEntity<AuthorizationResponseDTO> response = restTemplate.getForEntity(url,
+                    AuthorizationResponseDTO.class);
+
+            if ("success".equalsIgnoreCase(response.getBody().getStatus())
+                    && response.getBody().getData().getAuthorization()) {
+                return true;
+            } else {
+                return false;
+            }
+        } catch (Exception e) {
+            throw new RuntimeException("Erro ao enviar requisição ao serviço autorizador");
+        }
+
+    }
 }
