5ecdd58487f49126cead3f4335e28b89fbcbab46
bbering <bbering88@gmail.com>
2025-04-27 23:06:50 -0300

    refactor(NotifyDataDTO): refatorada a DTO contendo os dados da requisição que será enviada ao serviço externo de notificação

--- src/main/java/com/bbering/picpay_desafio_backend/dtos/NotifyDataDTO.java
+++ src/main/java/com/bbering/picpay_desafio_backend/dtos/NotifyDataDTO.java
@@ -1,10 +1,14 @@
 package com.bbering.picpay_desafio_backend.dtos;
 
+import java.math.BigDecimal;
+
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
 public class NotifyDataDTO {
-    private String message;
+    private Long senderId;
+    private Long receiverId;
+    private BigDecimal amount;
 }
