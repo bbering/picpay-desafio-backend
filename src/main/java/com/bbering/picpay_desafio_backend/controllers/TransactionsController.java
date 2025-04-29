5db248a97903085f0d15a225dd05e7244407e239
bbering <bbering88@gmail.com>
2025-04-28 22:24:56 -0300

    feat(TransactionsController): adicionadas anotações de documentação na controller

--- src/main/java/com/bbering/picpay_desafio_backend/controllers/TransactionsController.java
+++ src/main/java/com/bbering/picpay_desafio_backend/controllers/TransactionsController.java
@@ -12,15 +12,26 @@ import com.bbering.picpay_desafio_backend.dtos.TransactionRequestDTO;
 import com.bbering.picpay_desafio_backend.dtos.TransactionResponseDTO;
 import com.bbering.picpay_desafio_backend.services.TransactionsService;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 
 @RestController
 @RequestMapping("/api/v1")
+@Tag(name = "Transferências", description = "Operações relacionadas à transferências")
 public class TransactionsController {
 
     @Autowired
     private TransactionsService transactionsService;
 
+    @Operation(summary = "Realizar uma nova transferência", description = "Cria uma nova transação")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "201", description = "Transferência criada com sucesso"),
+            @ApiResponse(responseCode = "400", description = "Dados inválidos"),
+            @ApiResponse(responseCode = "500", description = "Erro no servidor")
+    })
     @PostMapping("/transfer")
     public ResponseEntity<TransactionResponseDTO> postANewTransaction(
             @Valid @RequestBody TransactionRequestDTO transactionData) {
